f7621fd321238cd88e9c5e9eb6ee366e10fbd1b9
Sungmin Lee <sungminlee417@gmail.com>
2023-06-08 15:47:48 -0700

    working on about page

--- src/About.js
+++ src/About.js
@@ -1,52 +1,109 @@
-import React from 'react';
+import React from "react";
+import InViewMotion from "./InViewMotion";
 
-function About(props) {
-    return (
-<>
-    <div class="py-4 lg:py-24">
-       <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl flex justify-center mb-4 mt-2">
-        Who We Are
-      </h1>
-    <div class="flex justify-center px-4">
-    <div class="grid grid-cols-1 justify-items-center max-w-7xl">
-    <div>
-      <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl mb-2 flex justify-center">
-        Our Mission
-      </h2>
-       <p>
-        We understand the importance and struggles of truly expressing an organization digitally. Echo Web LLC exists to help organizations truly represent their mission and values through a strong online presence. We design and develop anything from standard brochures to complex 3D E-commerce websites.
-      </p>
+function About() {
+  return (
+    <div className="flex justify-center md:justify-start py-10 lg:pt-56 lg:pb-20">
+      <div className="flex flex-col lg:gap-36 md:gap-24 gap-16 md:ml-12 ml-0 md:w-[80%] w-[90%]">
+        <div className="flex flex-col md:gap-14 gap-8">
+          <InViewMotion>
+            <h2 className="text-4xl md:text-6xl font-extrabold text-gray-900 lg:text-8xl flex">
+              Who We Are
+            </h2>
+          </InViewMotion>
+          <InViewMotion>
+            <p className="lg:text-2xl md:text-xl text-lg">
+              We are not just another web design agency. We are the storytellers
+              of the digital world, weaving captivating narratives through
+              stunning visuals and immersive experiences. From the moment
+              visitors land on your website, we make sure they are spellbound by
+              your brand's unique story, creating connections that last a
+              lifetime.
+            </p>
+          </InViewMotion>
+        </div>
+        <div className="flex flex-col md:gap-14 gap-8">
+          <InViewMotion>
+            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-2 flex">
+              Our Mission
+            </h2>
+          </InViewMotion>
+          <InViewMotion>
+            <p className="md:text-2xl text-lg">
+              Our mission is to empower organizations to leave a lasting
+              impression in the digital realm. We believe in the transformative
+              power of a strong online presence and the ability to express your
+              mission and values through cutting-edge design and innovative
+              technology. We don't just build websites; we craft digital
+              experiences that spark curiosity, evoke emotion, and inspire
+              action. Let us help you unleash the full potential of your brand
+              in the digital landscape.
+            </p>
+          </InViewMotion>
+        </div>
+        <div className="flex flex-col md:gap-14 gap-8 justify-center">
+          <InViewMotion>
+            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-2 flex">
+              Our Values
+            </h2>
+          </InViewMotion>
+          <InViewMotion>
+            <p className="md:text-2xl text-lg">
+              At Echo Web LLC, we live and breathe our core values. They are the
+              guiding principles that drive us forward and shape every project
+              we undertake:
+            </p>
+          </InViewMotion>
+          <ul className="flex flex-col gap-6 md:text-2xl text-lg">
+            <InViewMotion>
+              <li>
+                <span className="font-semibold">Unleash Creativity:</span> We
+                embrace the boundless power of creativity to craft extraordinary
+                digital experiences. Our team of visionaries pushes the
+                boundaries of design and technology to deliver solutions that
+                leave a lasting impact.
+              </li>
+            </InViewMotion>
+            <InViewMotion>
+              <li>
+                <span className="font-semibold">Deliver Excellence:</span> We
+                strive for excellence in everything we do. From pixel-perfect
+                designs to flawless functionality, we obsess over the details to
+                ensure that every project we deliver is of the highest quality.
+              </li>
+            </InViewMotion>
+            <InViewMotion>
+              <li>
+                <span className="font-semibold">Ignite Collaboration:</span> We
+                believe that great ideas are born from collaboration. By
+                fostering an environment of trust, respect, and open
+                communication, we collaborate closely with our clients to bring
+                their visions to life, transforming ideas into reality.
+              </li>
+            </InViewMotion>
+            <InViewMotion>
+              <li>
+                <span className="font-semibold">Inspire Innovation:</span> We
+                thrive on pushing the boundaries of what's possible. Our team
+                stays at the forefront of industry trends and emerging
+                technologies, constantly seeking innovative solutions to propel
+                your brand ahead of the competition.
+              </li>
+            </InViewMotion>
+          </ul>
+          <InViewMotion>
+            <p className="md:text-2xl text-lg">
+              These values are not just words on a wall; they are the pillars of
+              our agency. We invite you to embark on a digital journey with us,
+              where creativity knows no bounds, excellence is a standard, and
+              collaboration fuels remarkable innovation.
+            </p>
+          </InViewMotion>
+        </div>
+        <div>CTA INSERTED HERE</div>
       </div>
     </div>
-    </div>
-    <br/>
-    <div class="flex justify-center px-4">
-    <div class="grid grid-cols-1 justify-items-center max-w-7xl">
-    <div>
-      <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl mb-2 flex justify-center">
-        Our Values
-      </h2>
-       <p>
-        At Echo Web LLC, we are committed to upholding the following values:
-        <ul>
-            <br/>
-            <li> <span class="font-semibold"> Embracing New Challenges: </span> We believe in pushing ourselves to continuously learn and grow, both individually and as a team. We actively seek out new and diverse challenges to help us improve and innovate.</li>
-            <br/>
-            <li> <span class="font-semibold"> Celebrating Diversity: </span>We value diversity and inclusivity in all its forms. We recognize that our differences are what make us stronger and we strive to create a welcoming and supportive environment for everyone.</li>
-            <br/>
-            <li> <span class="font-semibold">Fostering Community:</span> We believe in the power of collaboration and teamwork. We work together to build strong relationships with our clients, partners, and each other, and we actively contribute to the communities we serve.</li>
-            <br/>
-            <li><span class="font-semibold">Supporting Non-Profits and Eco-Friendly Businesses: </span> We are committed to making a positive impact on the world around us. We actively seek out and support businesses that prioritize social and environmental responsibility.</li>
-        </ul>
-        <br/>
-        These values guide us in everything we do, from how we work with clients to how we treat each other.
-      </p>
-      </div>
-    </div>
-    </div>
-    </div>  
-</>
-    );
+  );
 }
 
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,40 +1,76 @@
-import './App.css';
-import Footer from './Footer';
-import Landing from './Landing';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './Nav';
-import Brochure from './Brochure';
-import QuickQuote from './QuickQuote';
-import Error from './Error';
-import About from './About';
-import ContactUs from './ContactUs';
-import { PagebloxDndProvider } from 'pageblox-react'
+import "./App.css";
+import Footer from "./Footer";
+import Landing from "./Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./Nav";
+import Brochure from "./Brochure";
+import QuickQuote from "./QuickQuote";
+import Error from "./Error";
+import About from "./About";
+import ContactUs from "./ContactUs";
+import { PagebloxDndProvider } from "pageblox-react";
 
 function App() {
-//SEO
+  //SEO
   //React Helmet
-  //Proper tags 
+  //Proper tags
   //SEO validation and check
-//PPolicy
-//SSL
-//Remove unused npm-prune
-//React Production build and "react validation?""
-//ERROR PAGES
+  //PPolicy
+  //SSL
+  //Remove unused npm-prune
+  //React Production build and "react validation?""
+  //ERROR PAGES
   return (
     <>
-    <PagebloxDndProvider clientId="playgroundKey">
-    <BrowserRouter>
-    <Nav></Nav>
-    <Routes>
-    <Route path="/" element={<><Landing></Landing><Brochure></Brochure></>}></Route>
-    <Route path="/quickquote" element={<><QuickQuote></QuickQuote></>}></Route>
-    <Route path="/about" element={<><About></About></>}></Route>
-    <Route path="/contact" element={<><ContactUs></ContactUs></>}></Route>
-    <Route path="/404" element={<><Error></Error></>}></Route>
-    </Routes>
-    <Footer></Footer>
-    </BrowserRouter>
-    </PagebloxDndProvider>
+      <PagebloxDndProvider clientId="playgroundKey">
+        <BrowserRouter>
+          <Nav></Nav>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Landing></Landing>
+                  <Brochure></Brochure>
+                </>
+              }
+            ></Route>
+            <Route
+              path="/quickquote"
+              element={
+                <>
+                  <QuickQuote></QuickQuote>
+                </>
+              }
+            ></Route>
+            <Route
+              path="/about"
+              element={
+                <>
+                  <About></About>
+                </>
+              }
+            ></Route>
+            <Route
+              path="/contact"
+              element={
+                <>
+                  <ContactUs></ContactUs>
+                </>
+              }
+            ></Route>
+            <Route
+              path="/404"
+              element={
+                <>
+                  <Error></Error>
+                </>
+              }
+            ></Route>
+          </Routes>
+          <Footer></Footer>
+        </BrowserRouter>
+      </PagebloxDndProvider>
     </>
   );
 }
